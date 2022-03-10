820b9dd764c57acfb465a625bc700e720d45914a
NilvanPeres <nilvanperes@gmail.com>
2022-03-10 18:59:55 -0300

    add triage questions
    Co-authored-by: yuriAlves5 <yurivalerio12@gmail.com>
    Co-authored-by: zpeniel09  <zpeniel09@gmail.com>
    Co-authored-by: mibasFerraz <lucasllff@gmail.com>
    Co-authored-by: fmaachadoo <filipe__machado@hotmail.com>

--- triage_category.pl
+++ triage_category.pl
@@ -23,6 +23,49 @@ category(respiratoryInfection, green).
 
 
 
+% Triage questions
+
+getIdade :- 
+    write('Type the age of patient: '),
+    read(Idade),
+    assert(idade(Idade)).
+
+getPainScale :- 
+    write('Type the scale of patient: '),
+    read(PainScale),
+    assert(painScale(PainScale)).
+
+isBleeding :- 
+    write('Is patient bleeding? (yes/no): '),
+    read(Bleeding),
+    assert(bleeding(Bleeding)).
+
+isFeverish :- 
+    write('Is patient feverish? (yes/no): '),
+    read(Feverish),
+    assert(feverish(Feverish)).
+
+isVomiting :- 
+    write('Is patient vomiting? (yes/no): '),
+    read(Vomit),
+    assert(vomit(Vomit)).
+
+isPregnant :- 
+    write('Is patient pregnant? (yes/no): '),
+    read(Pregnant),
+    assert(pregnite(Pregnant)).
+
+getPressure :- 
+    write('Type the pressure of patient: '),
+    read(Pressure),
+    assert(pressure(Pressure)).
+
+hasChestPain :- 
+    write('Is patient chest pain? (yes/no): '),
+    read(ChestPain),
+    assert(chestPain(ChestPain)).
+
+
 
 
 
