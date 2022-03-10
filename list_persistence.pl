e857a488b5665473810f1b097a04eb82d14916ec
Filipe Machado <filipe__machado@hotmail.com>
2022-03-10 15:36:03 -0300

    Adiciona timestamp em objeto de paciente
    Co-authored-by: ErickGiffoni <giffoni.erick@gmail.com>

--- list_persistence.pl
+++ list_persistence.pl
@@ -1,3 +1,4 @@
+:- use_module(library(dialect/sicstus/system)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/json)).
 :- use_module(library(lists)).
@@ -8,7 +9,8 @@ insert_patient_to_queue(_{nome:N}) :-
     open(FPath, read, StreamIn),
     json_read_dict(StreamIn, DictIn),
     close(StreamIn),
-    append(DictIn.queue, [_{nome:N}], PatientList),
+    now(T),
+    append(DictIn.queue, [_{nome:N, arrival_time:T}], PatientList),
     DictOut = DictIn.put(queue, PatientList),
     tell(FPath),
     json_write_dict(current_output, DictOut, [null('')]),
