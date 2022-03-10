20f82da390871d9dbee390077956b9b7bc88394b
Filipe Machado <filipe__machado@hotmail.com>
2022-03-10 14:29:12 -0300

    chore: remove prints de debug
    Co-authored-by: ErickGiffoni <giffoni.erick@gmail.com>

--- list_persistence.pl
+++ list_persistence.pl
@@ -8,34 +8,8 @@ insert_patient_to_queue({nome:N}) :-
     open(FPath, read, StreamIn),
     json_read_dict(StreamIn, DictIn),
     close(StreamIn),
-    write("PatientList: "),
-    write(PatientList), nl,
-    write("DictIn: "),
-    write(DictIn), nl,
-    
-    
     append(DictIn.queue, [_{nome:N}], PatientList),
-    
-    
-    write("JsonPatient: "),
-    write(Patient), nl,
-    write("DictIn: "),
-    write(DictIn), nl,
-    write("Patient: "),
-    write(Patient), nl,
-    write("PatientList: "),
-    write(PatientList), nl,
-    write("DictIn.queue: "),
-    write(DictIn.queue), nl,
-    
-    
     DictOut = DictIn.put(queue, PatientList),
-    
-    
-    write("DictOut: "),
-    write(DictOut),nl,
-    write("DictIn.queue: "),
-    write(DictIn.queue), nl,
     tell(FPath),
     json_write_dict(current_output, DictOut, [null('')]),
     told.
