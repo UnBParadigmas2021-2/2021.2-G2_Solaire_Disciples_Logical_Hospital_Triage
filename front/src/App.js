1745a98e0e19824387a305c9fd0828d374d994a7
freitasc <caina2000@gmail.com>
2022-03-10 22:49:46 -0300

    add api calling functions
    Co-authored-by: fmaachadoo <filipe__machado@hotmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,67 @@
-import React, { lazy, useEffect, useContext, useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, useEffect, useContext, useState } from "react";
+import api from "./services/api";
+import logo from "./logo.svg";
+import "./App.css";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [ManchesterPatientList, setManchesterPatientList] = useState();
+  const [RelativePatientList, setRelativePatientList] = useState();
+  const [ArrivalPatientList, setArrivalPatientList] = useState();
+
+  const getManchesterOrderList = async () => {
+    let patientList = null;
+    await api
+      .get("/get-patients/manchester-order")
+      .then((result) => {
+        patientList = result.data;
+      })
+      .catch((error) => {
+        patientList = {};
+      });
+    return patientList;
+  };
+
+  const getRelativeOrderList = async () => {
+    let patientList = null;
+    await api
+      .get("/get-patients/relative-order")
+      .then((result) => {
+        patientList = result.data;
+      })
+      .catch((error) => {
+        patientList = {};
+      });
+    return patientList;
+  };
+
+  const getArrivalOrderList = async () => {
+    let patientList = null;
+    await api
+      .get("/get-patients/arrival-order")
+      .then((result) => {
+        patientList = result.data;
+      })
+      .catch((error) => {
+        patientList = {};
+      });
+    return patientList;
+  };
+
+  const getData = async() => {
+    setManchesterPatientList(getManchesterOrderList());
+    setRelativePatientList(getRelativeOrderList());
+    setArrivalPatientList(getArrivalOrderList());
+  }
 
   useEffect(() => {
-    if (isLoading === true){
-      
+    if (isLoading === true) {
+      getData()
+      if(ManchesterPatientList && RelativePatientList && ArrivalPatientList){
+        setIsLoading(false)
+      }
     }
-  }, [])
+  });
 
   return (
     <>
@@ -17,21 +69,20 @@ export default function App() {
         <div>Carregando...</div>
       ) : (
         <>
-          <div className="App" style={{ background: 'linear-gradient(yellow,black)' }}>
+          <div
+            className="App"
+            style={{ background: "linear-gradient(yellow,black)" }}
+          >
             <div className="Header-Container">
               <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Hospital Warrior of Sunlight
-                </p>
+                <p>Hospital Warrior of Sunlight</p>
               </header>
             </div>
 
             <div className="App-container">
-              <div className="List-Patients">
+              <div className="List-Patients"></div>
 
-              </div>
-              
               <div className="Add-Patients">
                 <form>
                   <label>
@@ -47,12 +98,10 @@ export default function App() {
                 </form>
                 <input type="submit" value="Enviar" />
               </div>
-
             </div>
           </div>
         </>
       )}
     </>
   );
-
 }
