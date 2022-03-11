910a9013b1356a1a4daa0947c020e44e7c02a1c4
freitasc <caina2000@gmail.com>
2022-03-10 21:53:38 -0300

    add basic form
    Co-authored-by: fmaachadoo <filipe__machado@hotmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: 'linear-gradient(yellow,black)' }}>
+      <div className="Header-Container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hospital Warrior of Sunlight
+          </p>
+        </header>
+      </div>
+
+      <div className="App-container">
+        <div className="List-Patients">
+          <form>
+            <label>
+              Nome:
+              <input type="text" name="name" />
+            </label>
+          </form>
+          <form>
+            <label>
+              Prioridade:
+              <input type="text" name="name" />
+            </label>
+          </form>
+          <input type="submit" value="Enviar" />
+        </div>
+        
+        <div className="Add-Patients">
+
+        </div>
+      </div>
     </div>
   );
 }
