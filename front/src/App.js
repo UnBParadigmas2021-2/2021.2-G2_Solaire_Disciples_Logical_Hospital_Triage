048749ee40efdc149f99087958aa080337fc2ef0
freitasc <caina2000@gmail.com>
2022-03-10 22:14:30 -0300

    add axios and api calling
    Co-authored-by: fmaachadoo <filipe__machado@hotmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,41 +1,58 @@
+import React, { lazy, useEffect, useContext, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (isLoading === true){
+      
+    }
+  }, [])
+
   return (
-    <div className="App" style={{ background: 'linear-gradient(yellow,black)' }}>
-      <div className="Header-Container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hospital Warrior of Sunlight
-          </p>
-        </header>
-      </div>
+    <>
+      {isLoading ? (
+        <div>Carregando...</div>
+      ) : (
+        <>
+          <div className="App" style={{ background: 'linear-gradient(yellow,black)' }}>
+            <div className="Header-Container">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Hospital Warrior of Sunlight
+                </p>
+              </header>
+            </div>
 
-      <div className="App-container">
-        <div className="List-Patients">
-          <form>
-            <label>
-              Nome:
-              <input type="text" name="name" />
-            </label>
-          </form>
-          <form>
-            <label>
-              Prioridade:
-              <input type="text" name="name" />
-            </label>
-          </form>
-          <input type="submit" value="Enviar" />
-        </div>
-        
-        <div className="Add-Patients">
+            <div className="App-container">
+              <div className="List-Patients">
 
-        </div>
-      </div>
-    </div>
+              </div>
+              
+              <div className="Add-Patients">
+                <form>
+                  <label>
+                    Nome:
+                    <input type="text" name="name" />
+                  </label>
+                </form>
+                <form>
+                  <label>
+                    Prioridade:
+                    <input type="text" name="name" />
+                  </label>
+                </form>
+                <input type="submit" value="Enviar" />
+              </div>
+
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
-}
 
-export default App;
+}
