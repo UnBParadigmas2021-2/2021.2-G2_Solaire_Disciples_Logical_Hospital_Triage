2554762d3c47995ddc82f5e367545ff254d069d9
Filipe Machado <filipe__machado@hotmail.com>
2022-03-14 00:15:30 -0300

    Adiciona determinaçao do protocolo de manchester baseado em [link](https://github.com/EscolaDeSaudePublica/devLab/issues/7)
    Co-authored-by: zpeniel09  <zpeniel09@gmail.com>
    Co-authored-by: ErickGiffoni <giffoni.erick@gmail.com>
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>
    Co-authored-by: mibasFerraz <lucasllff@gmail.com>
    Co-authored-by: yuriAlves5 <yurivalerio12@gmail.com>
    Co-authored-by: omateusp <o.mateus.p@gmail.com>

--- triage_category.pl
+++ triage_category.pl
@@ -167,27 +167,51 @@ triage_category(ManchesterPriority) :-
     write(ManchesterPriority).
 
 get_manchester_priority(
+    Age,
     BadBreathing,
     BleedingLevel,
     ShockState,
     Convulsioning,
-    NotRespondingChild,
     PainLevel,
     Unconscious,
     BodyTemperature,
     UnconsciousHistory,
-    Adult,
     MinorRecentProblem,
     ManchesterPriority
 ) :-
-    ManchesterPriority is 5.
-
+    !,  (BadBreathing 
+        ; extremeBleeding(BleedingLevel) 
+        ; ShockState
+        ; Convulsioning
+        ; (isChild(Age), Unconscious)) 
+        -> ManchesterPriority is 1
+    ;   (isHighPain(PainLevel)
+        ;highBleeding(BleedingLevel)
+        ;Unconscious
+        ; (isChild(Age), isTemperatureHigh(BodyTemperature))
+        ; isTemperatureLow(BodyTemperature)
+        ; isMediumPain(PainLevel))
+        -> ManchesterPriority is 2
+    ;   (lowBleeding(BleedingLevel)
+        ; UnconsciousHistory
+        ; isTemperatureHigh(BodyTemperature)) 
+        -> ManchesterPriority is 3
+    ;   (isLowPain(PainLevel)
+        ; isLowFeverish(BodyTemperature)
+        ; MinorRecentProblem)
+        -> ManchesterPriority is 4
+    ;   ManchesterPriority is 5.
+    
         
 isTemperatureHigh(BodyTemperature) :-
-    BodyTemperature >= 38 -> true.
+    BodyTemperature > 38 -> true.
 
 isTemperatureLow(BodyTemperature) :-
     35 >= BodyTemperature -> true.
+
+isLowFeverish(BodyTemperature) :-
+    BodyTemperature =:= 38 -> true.
+
 
 isChild(Age):-
     12 >= Age -> true.
@@ -196,10 +220,23 @@ isOxigenLow(OxigenLevel):-
     89 >= OxigenLevel -> true. 
 
 isNoPain(PainLevel):-
-    PainLevel =:= 1 -> true.
+    PainLevel =:= 0 -> true.
 
 isLowPain(PainLevel):-
-    PainLevel =:= 2 -> true.
+    PainLevel =:= 1 -> true.
+
+isMediumPain(PainLevel):-
+    PainLevel =:= 1 -> true.
 
 isHighPain(PainLevel):-
     PainLevel =:= 3 -> true.
+
+extremeBleeding(BleedingLevel):-
+    BleedingLevel =:= 3 -> true.
+
+highBleeding(BleedingLevel):-
+    BleedingLevel =:= 2 -> true.
+
+lowBleeding(BleedingLevel):-
+    BleedingLevel =:= 1 -> true.
+
